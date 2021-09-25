6bd7b3fed1c6151e70f82b1060bf9c90e3d6eea3
t104360088 <z8631989816@gmail.com>
2021-09-25 11:50:41 +0800

    Import Firebase Crashlytics

--- app/src/main/java/com/example/mytimber/MainActivity.kt
+++ app/src/main/java/com/example/mytimber/MainActivity.kt
@@ -10,5 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Timber.d("Hello Timber")
+
+        throw RuntimeException("Test") // Force a crash
     }
 }
